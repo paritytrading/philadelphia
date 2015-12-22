6390765ddc60fc75d4c2f153f5f4ecef740ebab4
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-12-23 00:13:09 +0200

    Add constant for FIX 4.4 to version enumeration

--- philadelphia/src/main/java/org/jvirtanen/philadelphia/FIXVersion.java
+++ philadelphia/src/main/java/org/jvirtanen/philadelphia/FIXVersion.java
@@ -6,7 +6,8 @@ package org.jvirtanen.philadelphia;
 public enum FIXVersion {
 
     FIX_4_2("FIX.4.2"),
-    FIX_4_3("FIX.4.3");
+    FIX_4_3("FIX.4.3"),
+    FIX_4_4("FIX.4.4");
 
     private String beginString;
 
