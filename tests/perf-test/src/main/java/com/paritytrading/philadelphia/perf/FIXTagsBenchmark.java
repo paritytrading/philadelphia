35d6b9e7d9ce38309261d7c6cd6fecd57a9cd3c6
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2017-05-07 14:08:17 +0200

    Configure iterations and forks in performance test

--- tests/perf-test/src/main/java/com/paritytrading/philadelphia/perf/FIXTagsBenchmark.java
+++ tests/perf-test/src/main/java/com/paritytrading/philadelphia/perf/FIXTagsBenchmark.java
@@ -7,14 +7,20 @@ import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
 
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 3)
 @State(Scope.Benchmark)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.SampleTime)
