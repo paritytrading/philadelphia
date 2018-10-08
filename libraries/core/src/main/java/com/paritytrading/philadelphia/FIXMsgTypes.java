0e4331475f58e751699748011ef1d8235f086aa2
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2018-10-08 22:27:35 +0100

    philadelphia-core: Remove superfluous whitespace

--- libraries/core/src/main/java/com/paritytrading/philadelphia/FIXMsgTypes.java
+++ libraries/core/src/main/java/com/paritytrading/philadelphia/FIXMsgTypes.java
@@ -2,12 +2,12 @@ package com.paritytrading.philadelphia;
 
 class FIXMsgTypes {
 
-    public static final char Heartbeat       = '0';
-    public static final char TestRequest     = '1';
-    public static final char ResendRequest   = '2';
-    public static final char Reject          = '3';
-    public static final char SequenceReset   = '4';
-    public static final char Logout          = '5';
-    public static final char Logon           = 'A';
+    public static final char Heartbeat     = '0';
+    public static final char TestRequest   = '1';
+    public static final char ResendRequest = '2';
+    public static final char Reject        = '3';
+    public static final char SequenceReset = '4';
+    public static final char Logout        = '5';
+    public static final char Logon         = 'A';
 
 }
