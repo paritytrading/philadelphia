4f48e501b3c2f4047a33ca618f89f9ebdf4c87ba
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2018-10-10 17:48:18 +0100

    philadelphia-perf-test: Clean up configuration

--- tests/perf-test/src/main/java/com/paritytrading/philadelphia/perf/FIXTimestampsBenchmark.java
+++ tests/perf-test/src/main/java/com/paritytrading/philadelphia/perf/FIXTimestampsBenchmark.java
@@ -1,27 +1,12 @@
 package com.paritytrading.philadelphia.perf;
 
 import com.paritytrading.philadelphia.FIXTimestamps;
-import java.util.concurrent.TimeUnit;
 import org.joda.time.MutableDateTime;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
 
-@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Fork(value = 3)
-@State(Scope.Benchmark)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
-@BenchmarkMode(Mode.SampleTime)
-public class FIXTimestampsBenchmark {
+public class FIXTimestampsBenchmark extends FIXBenchmark {
 
     private StringBuilder builder;
 
