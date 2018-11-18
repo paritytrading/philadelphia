08f1929ea6e51c002ccb8cefe4a8140b39e8d626
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2018-11-18 12:43:51 +0000

    philadelphia-core: Clean up 'FIXMsgTypes'
    Remove superfluous 'public' modifiers.

--- libraries/core/src/main/java/com/paritytrading/philadelphia/FIXMsgTypes.java
+++ libraries/core/src/main/java/com/paritytrading/philadelphia/FIXMsgTypes.java
@@ -2,12 +2,12 @@ package com.paritytrading.philadelphia;
 
 class FIXMsgTypes {
 
-    public static final char Heartbeat     = '0';
-    public static final char TestRequest   = '1';
-    public static final char ResendRequest = '2';
-    public static final char Reject        = '3';
-    public static final char SequenceReset = '4';
-    public static final char Logout        = '5';
-    public static final char Logon         = 'A';
+    static final char Heartbeat     = '0';
+    static final char TestRequest   = '1';
+    static final char ResendRequest = '2';
+    static final char Reject        = '3';
+    static final char SequenceReset = '4';
+    static final char Logout        = '5';
+    static final char Logon         = 'A';
 
 }
