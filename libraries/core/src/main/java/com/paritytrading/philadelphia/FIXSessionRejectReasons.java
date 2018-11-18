aad65754a9dd9a881a529fd8123ba786ce82f270
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2018-11-18 12:44:36 +0000

    philadelphia-core: Clean up 'FIXSessionRejectReasons'
    Remove superfluous 'public' modifiers.

--- libraries/core/src/main/java/com/paritytrading/philadelphia/FIXSessionRejectReasons.java
+++ libraries/core/src/main/java/com/paritytrading/philadelphia/FIXSessionRejectReasons.java
@@ -2,7 +2,7 @@ package com.paritytrading.philadelphia;
 
 class FIXSessionRejectReasons {
 
-    public static final int RequiredTagMissing = 1;
-    public static final int ValueIsIncorrect   = 5;
+    static final int RequiredTagMissing = 1;
+    static final int ValueIsIncorrect   = 5;
 
 }
