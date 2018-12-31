61e813f556ec8ffff635e1068369cebdffb8aef3
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2018-12-31 09:08:24 +0000

    philadelphia-client: Clean up 'Command'
    Remove superfluous 'public' modifiers.

--- applications/client/src/main/java/com/paritytrading/philadelphia/client/Command.java
+++ applications/client/src/main/java/com/paritytrading/philadelphia/client/Command.java
@@ -3,7 +3,7 @@ package com.paritytrading.philadelphia.client;
 import java.io.IOException;
 import java.util.Scanner;
 
-public interface Command {
+interface Command {
 
     void execute(TerminalClient client, Scanner arguments) throws CommandException, IOException;
 
