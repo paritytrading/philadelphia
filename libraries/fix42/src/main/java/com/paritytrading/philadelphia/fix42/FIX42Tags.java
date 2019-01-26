e83f7e9ecb7d0a53ea74d3d3f0be6552bc4cfd6d
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2019-01-26 17:44:59 +0000

    philadelphia-fix42: Update source files

--- libraries/fix42/src/main/java/com/paritytrading/philadelphia/fix42/FIX42Tags.java
+++ libraries/fix42/src/main/java/com/paritytrading/philadelphia/fix42/FIX42Tags.java
@@ -1,5 +1,12 @@
 package com.paritytrading.philadelphia.fix42;
 
+/*
+ * This file has been automatically generated using Philadelphia Code
+ * Generator. For more information on Philadelphia Code Generator, see:
+ *
+ *   https://github.com/paritytrading/philadelphia
+ */
+
 /**
  * Tags for FIX 4.2.
  */
